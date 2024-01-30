707d92ee73e1694404cbed7d053402091a86de42
Parijat Bose <torontoloaner1@torontos-mbp.home>
2024-01-29 19:40:15 -0500

    working version

---
+++ resources/sql/financial_data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE financial_data (
+    month VARCHAR(50),
+    rent_revenue DECIMAL(10, 2),
+    cam_revenue DECIMAL(10, 2),
+    insurance_revenue DECIMAL(10, 2),
+    property_tax_revenue DECIMAL(10, 2),
+    landscaping_contract DECIMAL(10, 2),
+    electricity_expense DECIMAL(10, 2),
+    gas_expense DECIMAL(10, 2),
+    water_drainage_expense DECIMAL(10, 2),
+    water_sewer_expense DECIMAL(10, 2),
+    property_insurance_expense DECIMAL(10, 2),
+    property_tax_expense DECIMAL(10, 2),
+    property_tax_consultant DECIMAL(10, 2),
+    management_fee_expense DECIMAL(10, 2),
+    accounting_service_expense DECIMAL(10, 2),
+    legal_services DECIMAL(10, 2),
+    note_1_interest DECIMAL(10, 2)
+);
